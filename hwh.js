0616acaddb553e58c51b0b0948c2a3dfc2f43afd
Spencer Young <spencerpjy@gmail.com>
2018-06-12 00:50:08 -1000

    Added rules, code clean up

--- hwh.js
+++ hwh.js
@@ -175,7 +175,9 @@ client.on('message', (message) => {
           .catch(err => console.error(err));
       }
 
-      message.delete();
+      message
+        .delete()
+        .catch(err => console.error(err));
     }
 
     /*
@@ -412,32 +414,32 @@ client.on('message', (message) => {
               description: 'In addition to following the [Discord Guidelines](http://dis.gd/guidelines), you must also comply with these rules while on Homework Help:',
               fields: [
                 {
-                  name: 'Quality of Life',
-                  value: '**1) Familiarize yourself with the rules** so you don\'t get in trouble.\n' +
-                    '**2) Introduce yourself** Say hello in <#238956364729155585> and talk a little about yourself!\n' +
-                    '**3) Get a shiny new role** to display what you can help the most with in <#275071813992710144>.\n' +
-                    '**4) Start a discussion or ask for help** in the respective channels.\n' +
-                    '**5) If you have a question, don\'t hesitate to ask it.** To save time, post it instead of asking "Does anyone know X?" or "Can someone help with Y?"\n' +
-                    '**6) Display your knowledge** in one of our events <#446166388235829248>.\n\u200b',
+                  name: 'A. Quality of Life',
+                  value: '**1) Respect others** — Keep in mind you are speaking to other people. Treat others how you want to be treated.\n' +
+                    '**2) Remain civil**  — Maintain civil conversations by displaying a placid demeanor. Don\'t personally attack anyone over their physicality or personality. Avoid over-excessive profanity.\n' +
+                    '**3) Don\'t send unsolicited direct messages** — As a courtesy to respect others\' privacy, don\'t send direct messages asking for help unless explicitly given permission.\n' +
+                    '**4) Discord name / nickname**  — Your name must precede with a minimum of three alphanumeric characters, remain within the bounds of the message line, and maintain an appropriate appearance.\n' +
+                    '**5) Use the `@HWH Staff` mention for reports only.** — In the report, please also include the offender and the offense.\n' +
+                    '**6) Don\'t backseat moderate** — Let staff members give the punishments. If you notice a rule breaker, submit a report (reference the previous point (A.5)).\n\u200b',
                 },
                 {
-                  name: 'Help Channels',
-                  value: '**1) Server Commands** —\n' +
-                  '• `@HWH Staff <@USER> <OFFENSE>` to anonymously submit a report.\n' +
-                  '• `?ta5` macro that quickly displays \'If you have a question, don\'t hesitate to ask it. To save time, post it instead of asking "Does anyone know X?" or "Can someone help with Y?"\'\n' +
-                  '**2) Direct Message Commands** —\n' +
-                  '• `>c <challenge id> <work>` for event submissions.\n\u200b',
+                  name: 'B. Messaging',
+                  value: '**1) Post clean content only**  — Keep the channels free of NSFW and illegal material.\n' +
+                  '**2) Don\'t spam** — Don\'t send repeating, similar or non-pertinent messages in quick succession.\n' +
+                  '**3) Don\'t advertise servers, services, or social media without permission** — We want to be sure to scan them for any inappropriate content before posting them.\n\u200b',
                 },
                 {
-                  name: 'C. MathBot',
-                  value: '**1) LaTeX Renderer** — You can beautify your math by converting LaTeX into beautiful images. Use `=tex your LaTeX code here`.\n' +
-                  '**2) Wolfram Alpha Queryer** — Use MathBot to query Wolfram Alpha for mathematical computations or other knowledge. Use `=wolf wolfram alpha query`. Only usable in #spam-channel.\n\u200b',
+                  name: 'C. Help Channels',
+                  value: '**1) Mentioning/Tagging Roles** — After posting your own question, wait a minimum of 15 minutes before using the helper mention. If nobody has replied by the 15 minutes, repost the question and include a helper mention with or after the question. Only use one ping per question (*exception*: if there is a matching sub-topic role, you may include the mentions in one message only, ex. `@Science @Physics`).\n' +
+                  '**2) Be serious and encouraging in all help channels.** — We want to promote a sense of credibility when receiving help. Additionally, be encouraging to others, as they are merely trying to learn new things.\n' +
+                  '**3) Don\'t cheat or encourage it** — Academic integrity is an ideology we uphold seriously, so do not attempt to encourage cheating. Instead, guide the person through the problem to gain intuition on how to solve it.\n' +
+                  '**4) Don\'t offer payment, goods, or services in exchange for having your work done.** — We don\'t want to be held liable for any transactions that occur on this server.\n\u200b',
                 },
               ],
               timestamp: new Date(),
               footer: {
                 icon_url: client.user.avatarURL,
-                text: 'Homework Help Bot',
+                text: 'NOTE: Staff members have the power to take affirmative action in order to maintain a peaceful environment. They have the final say in any situation.',
               },
             },
           }
