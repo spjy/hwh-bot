e4848f8ade5229c681e8254641401aefd897d9ee
Spencer Young <spencerpjy@gmail.com>
2018-06-26 22:30:46 -1000

    Moved env checking into .execute() function of dialogflow.js

--- events/message/dialogflow.js
+++ events/message/dialogflow.js
@@ -2,48 +2,46 @@ require('dotenv-extended').load();
 const Raven = require('raven');
 const dialogflow = require('dialogflow');
 
-(() => {
-  if (process.env.DIALOGFLOW_PROJECT_ID === undefined) {
-    console.warn('DialogFlow not configured.');
-    return;
-  }
+module.exports = {
+  description: 'DialogFlow',
+  execute(message) {
+    const {
+      cleanContent: content,
+      author
+    } = message;
 
-  const sessionClient = new dialogflow.SessionsClient({
-    projectId: process.env.DIALOGFLOW_PROJECT_ID,
-    credentials: {
-      client_email: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_CLOUD_PRIVATE_KEY.replace(/\\n/g, '\n')
+    if (process.env.DIALOGFLOW_PROJECT_ID === undefined) {
+      console.warn('DialogFlow not configured.');
+      return;
     }
-  });
 
-  module.exports = {
-    description: 'DialogFlow',
-    execute(message) {
-      const {
-        cleanContent: content,
-        author
-      } = message;
+    const sessionClient = new dialogflow.SessionsClient({
+      projectId: process.env.DIALOGFLOW_PROJECT_ID,
+      credentials: {
+        client_email: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_CLOUD_PRIVATE_KEY.replace(/\\n/g, '\n')
+      }
+    });
 
-      const sessionPath = sessionClient.sessionPath(
-        process.env.DIALOGFLOW_PROJECT_ID, author.id
-      );
+    const sessionPath = sessionClient.sessionPath(
+      process.env.DIALOGFLOW_PROJECT_ID, author.id
+    );
 
-      const request = {
-        session: sessionPath,
-        queryInput: {
-          text: {
-            text: String(content),
-            languageCode: 'en-US'
-          }
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: String(content),
+          languageCode: 'en-US'
         }
-      };
+      }
+    };
 
-      sessionClient.detectIntent(request)
-        .then((responses) => {
-          const result = responses[0].queryResult;
-          message.reply(result.fulfillmentText);
-        })
-        .catch(err => Raven.captureException(err));
-    }
-  };
-})();
+    sessionClient.detectIntent(request)
+      .then((responses) => {
+        const result = responses[0].queryResult;
+        message.reply(result.fulfillmentText);
+      })
+      .catch(err => Raven.captureException(err));
+  }
+};
