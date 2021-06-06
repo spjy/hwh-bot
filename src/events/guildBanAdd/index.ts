25443e3bb3d45a14916717d63c1d92d6bb6daba1
Spencer Young <spjy@hawaii.edu>
2021-06-05 23:26:32 -0700

    fix: file import checking

--- src/events/guildBanAdd/index.ts
+++ src/events/guildBanAdd/index.ts
@@ -6,12 +6,12 @@ import path from 'path';
 export default function (collection: Discord.Collection<string, any>) {
   // Get all of the scripts in directory and add it to a Discord collection
   fs.readdirSync(__dirname).forEach((file) => {
-    if (file === 'index.ts') return; // ignore index.js file
+    if (file === 'index.ts' || file === 'index.js') return; // ignore index.js file
 
     if (fs.lstatSync(path.format({ dir: __dirname, base: file })).isFile()
-      && file.endsWith('.ts')) {
+      && (file.endsWith('.ts') || file.endsWith('.js'))) {
       const directory = __dirname.split(path.sep); // get current folder name
-      const name = file.replace('.ts', ''); // get file name excluding .js
+      const name = file.endsWith('.ts') ? file.replace('.ts', '') : file.replace('.js', ''); // get file name excluding .js
       const event = require(`./${file}`); // get event file
 
       // Add event to Discord collection (Name format: event::filename)
