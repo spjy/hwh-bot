8c6b302239bdbb9248b2fb6f5836a970fc62976f
spjy <spjy@hawaii.edu>
2021-11-23 21:41:35 -0700

    feat: move reaction logic to report command

--- src/events/message/report.ts
+++ src/events/message/report.ts
@@ -1,6 +1,5 @@
 import Discord from 'discord.js'
 import Raven from 'raven';
-import Embed from '../../embed';
 
 /**
  * Detect when a user includes the @Staff ping, generate a report in the
@@ -32,37 +31,38 @@ export default class Report {
         author,
         guild,
         channel,
-        client
+        client,
+        mentions
       } = this.message;
 
       // Extract roles in message
-      const report = this.message.mentions.roles
+      const report = mentions.roles
         .map(role => role.id);
 
-      const indicator = new Discord.MessageEmbed({
-        color: 16645888,
-        description: '',
-        fields: [
-          {
-            name: 'Report',
-            value: 'Thank you for the report. We will review it shortly.',
-            inline: true
-          },
-          {
-            name: 'Staff Link',
-            value: '-',
-            inline: true
-          }
-        ],
-        timestamp: new Date(),
-        footer: {
-          icon_url: client.user.avatarURL(),
-          text: 'Homework Help'
-        }
-      })
-
-      // If mentions includes @Staff
+        // If mentions includes @Staff
       if (report.includes(this.staffRoleId)) {
+        const indicator = new Discord.MessageEmbed({
+          color: 16645888,
+          description: "",
+          fields: [
+            {
+              name: "Report",
+              value: "Thank you for the report. We will review it shortly.",
+              inline: true,
+            },
+            {
+              name: "Staff Link",
+              value: "-",
+              inline: true,
+            },
+          ],
+          timestamp: new Date(),
+          footer: {
+            icon_url: client.user.avatarURL(),
+            text: "Homework Help",
+          },
+        });
+
         const reportMessage = await (<Discord.TextChannel>guild.channels
           .cache
           .get(channel.id))
@@ -70,11 +70,20 @@ export default class Report {
             embeds: [indicator]
           });
 
+        const resolve = new Discord.MessageActionRow()
+          .addComponents(
+            new Discord.MessageButton()
+              .setCustomId('report::0')
+              .setLabel('Resolve Report')
+              .setStyle('DANGER'),
+          );
+
         // Send information to report channel in an embed
         const m = await (<Discord.TextChannel>guild.channels
           .cache
           .get(this.reportsChannel))
           .send({
+            components: [resolve],
             embeds: [
               new Discord.MessageEmbed({
                 color: 16645888,
@@ -92,6 +101,10 @@ export default class Report {
                     name: 'Channel',
                     value: `${channel}`,
                     inline: true
+                  },
+                  {
+                    name: 'Offender',
+                    value: 'No offender provided.'
                   },
                   {
                     name: 'Message',
@@ -117,45 +130,9 @@ export default class Report {
           .edit({
             embeds: [indicator]
           });
-
-        // React with a grin
-        await m
-          .react('😁');
-
-        await this.message
-          .delete();
-
-        await m
-          .awaitReactions({ filter: (reaction, user) => reaction.emoji.name === '😁', max: 1 });
-
-        await reportMessage
-          .edit({
-            embeds: [
-              new Discord.MessageEmbed({
-                color: 1441536,
-                description: '',
-                fields: [
-                  {
-                    name: 'Report',
-                    value: 'A staff member has reviewed your report. If you think there was a mistake, please contact us via <@575252669443211264>.',
-                    inline: true
-                  },
-                  {
-                    name: 'Staff Link',
-                    value: `[Case](${m.url})`,
-                    inline: true
-                  }
-                ],
-                timestamp: new Date(),
-                footer: {
-                  icon_url: client.user.avatarURL(),
-                  text: 'Homework Help'
-                }
-              })
-            ]
-        });
       }
     } catch (err) {
+      console.log(err)
       Raven.captureException(err);
     }
   }
