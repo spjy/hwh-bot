1bb4cfa9b8b60f3e29f974498c119beaca8e03cc
spjy <spjy@hawaii.edu>
2021-11-23 22:35:11 -0700

    feat: add mention commands

--- src/commands/mention.ts
+++ src/commands/mention.ts
@@ -18,16 +18,43 @@ export default class Report {
       .setDescription('Mention')
       .addSubcommand(subcommand =>
         subcommand
-          .setName('send')
-          .setDescription('Mention your selected message')
+          .setName('create')
+          .setDescription('Create a mention key to use in 15 minutes')
           .addRoleOption(option => option.setName('role').setDescription('Role to be mentioned').setRequired(true)))
       .addSubcommand(subcommand =>
         subcommand
+          .setName('overwrite')
+          .setDescription('Overwrite currently generated mention key with new role and message')
+          .addRoleOption(option => option.setName('role').setDescription('Role to be mentioned').setRequired(true)))
+      .addSubcommand(subcommand =>
+        subcommand
+          .setName('send')
+          .setDescription('Mention your selected message'))
+      .addSubcommand(subcommand =>
+        subcommand
           .setName('cancel')
-          .setDescription('Cancel current mention'));
+          .setDescription('Cancel current mention'))
   }
 
   async execute(interaction) {
-    interaction.reply({ content: 'mention' });
+    const { options, channel, message } = interaction;
+
+    interaction.reply({ content: 'Mention command not yet implemented.' });
+
+    // Allow mentions in help categories
+    // if (!channel.parent.name.toLowerCase().endsWith('help')) {
+    //   await interaction.reply({
+    //     content: 'You can only use mentions in help channels.'
+    //   });
+
+    //   return;
+    // }
+
+    // if (options.getSubcommand() === 'create') {
+    // } else if (options.getSubcommand() === 'overwrite') {
+    // } else if (options.getSubcommand() === 'send') {
+    //   const role = options.getRole('role');
+    // } else if (options.getSubcommand === 'cancel') {
+    // }
   }
 }
