fa98277f0877342fc11e6861f296fad1f60c7240
spjy <spjy@hawaii.edu>
2022-05-14 13:03:50 -0700

    feat: add note disposition

--- src/typedefs.ts
+++ src/typedefs.ts
@@ -1,18 +1,19 @@
 import Discord from 'discord.js';
 
 export interface MentionStore {
-  channel: string,
-  message: Discord.Message,
-  cooldownDate?: Date,
-  mention?: Discord.Role,
+  channel: string;
+  message: Discord.Message;
+  cooldownDate?: Date;
+  mention?: Discord.Role;
 }
 
 export enum dispositions {
   NO_ACTION = 'no_action',
+  NOTE = 'note',
   VERBAL_WARN = 'verbal_warn',
   FORMAL_WARN = 'formal_warn',
   MUTE = 'mute',
   KICK = 'kick',
   SOFTBAN = 'softban',
-  BAN = 'ban'
+  BAN = 'ban',
 }
